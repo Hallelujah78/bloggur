734df96c7e824d25d4fffeaf8c3dbcfab3e11fb8
Gavan Browne <gavan.browne@gmail.com>
2025-04-18 15:24:24 +0100

    Update index.html, styles.css and index.js
    HTML: Added link to style sheet. Add label text. Added a blog article container inside the blog container section.
    
    Index.js: Get and store the blog article container. Added call to createBlog to add blog button event listener. Added code to create a blog title, create a paragraph to hold the blog content. Added code to take content from the form and add it to the blog article. Added code to append the article to the blog container.
    
    Styles.css: Added a css reset. Add styles for various elements so we can distinguish them from each other when displayed.

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const titleDOM = document.querySelector(".title-input");
 const contentDOM = document.querySelector(".content-input");
 // Get the button.
 const addBlogBtn = document.querySelector(".add-blog");
+// Get the container for blog articles.
+const blogArticleContainerDOM = document.getElementById(
+  "blog-article-container"
+);
 
 // Add event listener to add button.
 addBlogBtn.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("hello world");
   // Create an article
+  createBlog();
 });
 
 // console.log(contentDOM);
@@ -25,4 +30,16 @@ const createBlog = () => {
   const article = document.createElement("article");
   // set article class name
   article.className = "blog-article";
+  // Create a heading for title
+  const title = document.createElement("h1");
+  // Create a paragraph for blog content.
+  const paragraph = document.createElement("p");
+  // Add title text.
+  title.textContent = titleDOM.value;
+  // Add blog content text.
+  paragraph.textContent = contentDOM.value;
+  // Append all the elements to the article.
+  article.append(title, paragraph);
+  // Append the article to the.
+  blogArticleContainerDOM.append(article);
 };
