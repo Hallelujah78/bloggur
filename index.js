22f12ff73587f5444d658945b3963f9e96c0cd8b
Gavan Browne <gavan.browne@gmail.com>
2025-04-19 16:44:10 +0100

    Update blog rendering + add pagination display
    Updated renderPosts - we now render posts based on the page number and the number of items per page. Each time the user adds a post, the blog posts container is cleared and the blog posts that relate to the current page number are rendered to the container.
    
    Added a updatePaginationDisplay method that simply hides or displays pagination based on whether the number of blog posts exceeds the items per page count.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Store the blog posts in an array.
 const articleArray = [];
 
+// Store the page for pagination.
+const pageNum = 1;
+
+// Store num items per page.
+const itemsPerPage = 8;
+
 // Get the title input.
 const titleDOM = document.querySelector(".title-input");
 // Get the text area input.
@@ -13,25 +19,29 @@ const blogArticleContainerDOM = document.getElementById(
 );
 // Get the form
 const formDOM = document.getElementById("blog-input-form");
+// Get the pagination container
+const paginationDOM = document.getElementById("blog-pagination");
 
 // Add event listener to add button.
 formDOM.addEventListener("submit", (e) => {
   e.preventDefault();
   // Create an article
-  createBlog();
+  const article = createBlog(titleDOM.value, contentDOM.value);
+  // Add article to article array.
+  articleArray.push(article);
+  // // Reset the title input.
+  // titleDOM.value = "";
+  // // Reset the textarea content
+  // contentDOM.value = "";
+  // Render posts.
+
+  renderPosts(itemsPerPage, articleArray, pageNum, blogArticleContainerDOM);
+  // Update pagination.
+  updatePaginationDisplay();
 });
 
-// Create new blog post function
-const createBlog = () => {
-  // get the title from form.
-  const titleValue = titleDOM.value;
-  // get the content from the form textarea.
-  const contentValue = contentDOM.value;
-  // Empty check.
-  if (titleValue === "" || contentValue === "") {
-    // if values are empty, warn user they must input title and content.
-  }
-
+// Function to create new blog post.
+const createBlog = (titleValue, contentValue) => {
   // create the article
   const article = document.createElement("article");
   // set article class name
@@ -46,6 +56,50 @@ const createBlog = () => {
   paragraph.textContent = contentValue;
   // Append all the elements to the article.
   article.append(title, paragraph);
-  // Append the article to the.
-  blogArticleContainerDOM.append(article);
+
+  // Return the article.
+  return article;
+};
+
+// Function to render blog posts.
+const renderPosts = (itemsPerPage, articleArray, pageNum, container) => {
+  console.log("num of articles: ", articleArray.length);
+  // Clear the container before rendering.
+  container.innerHTML = "";
+
+  // Calculate the indices of the start and ending posts to render.
+  const endVal = (pageNum - 1) * itemsPerPage; // pageNum 1, items 8, length 12 = 0 correct
+  // if on last page
+  if (pageNum === Math.ceil(articleArray.length / itemsPerPage)) {
+    // render items on last page
+    startValue =
+      // Is the last page full?
+      articleArray.length % itemsPerPage === 0
+        ? endVal + itemsPerPage - 1 // start value is last item on page.
+        : endVal + (articleArray.length % itemsPerPage) - 1; // start val is remaining items.
+  }
+  // else, we're not on last page, so render itemsPerPage items
+  else {
+    startValue = endVal + itemsPerPage - 1;
+  }
+
+  console.log("end val: ", endVal);
+  console.log("start val: ", startValue);
+
+  // Test.
+  for (let i = startValue; i >= endVal; i--) {
+    console.log(i);
+    // Append the blog post to the container.
+    container.append(articleArray[i]);
+  }
+};
+
+// Function to update pagination display.
+const updatePaginationDisplay = () => {
+  // If num blog posts less than or equal to itemsPerPage, hide pagination
+  if (articleArray.length <= itemsPerPage) {
+    paginationDOM.classList.add("hide");
+    return;
+  }
+  paginationDOM.classList.remove("hide");
 };
--- todo.md
+++ todo.md
@@ -22,8 +22,76 @@ remember:
 - as a user, I can add a new blog post.
 - new post is below old posts, but above the FORM
   - if we use a data structure that maintains order we don't have to timestamp each blog post.
-- input fields cleared on submission
+- input fields cleared on submission **DONE**
 
 - pagination element that is hidden if number of posts don't
 
-- clear blog fields on submit
+# 18-4-25
+
+## Thinking About Pagination
+
+- we have given values: the length of an array containing articles, the page number, the items to display per page.
+- blogs are displayed in reverse order.
+
+### Worked example 1:
+
+articleArray.length = 4, pageNum = 1, itemsPerPage = 8
+
+startingBlog = (articleArray.length % itemsPerPage) * pageNum - 1
+= (4 % 8)*1-1 = 3.
+CORRECT
+
+### Worked example 2:
+
+articleArray.length = 12, pageNum = 2, itemsPerPage = 8
+(articleArray.length % itemsPerPage) * pageNum - 1
+= (12 % 8)*2-1 = 7.
+CORRECT
+
+### Worked example 3:
+
+- Add items until we have 8 items, 0-7
+  articleArray.length = 12, pageNum = 2, itemsPerPage = 8
+  (articleArray.length % itemsPerPage) * pageNum - 1
+  = (12 % 8)*2-1 = 7.
+  CORRECT
+
+Ending Value
+i>=(pageNum-1)_itemsPerPage: 1-1 = 0 _ 8 where pageNum is 1
+i>=(pageNum-1)_itemsPerPage: 2-1 = 1 _ 8 where pageNum is 2
+
+Starting Value given pageNum, itemsPerPage and array.length
+starting value for i would be ((pageNum - 1) \* itemsPerPage) + array.length % itemsPerPage
+
+Worked Example: 2, 8 and 12
+((2-1) \* 8) + 12 % 8 = 8 + 4 = 12 (minus 1)
+
+Worked Example: 1, 8 and 12
+1-1 \* 8 = 0 + 4 = 4. incorrect.
+
+if pageNum is less than Math.ceil(array.length/itemsPerPage), then starting value is
+the end value + itemsPerPage
+
+if pageNum is equal to Math.ceil(array.length/itemsPerpage), starting value is array.length % itemsPerPage
+
+Worked example: 1, 8, 12
+
+```js
+const endVal = (pageNum - 1) * itemsPerPage; // pageNum 1, items 8, length 12 = 0 correct
+
+// if on last page
+if (pageNum === Math.ceil(articleArray.length / itemsPerPage)) {
+  // render items on last page
+  startValue =
+    // Is the last page full?
+    articleArray.length % itemsPerPage === 0
+      ? endVal + itemsPerPage - 1 // start value is last item on page.
+      : endVal + (articleArray.length % itemsPerPage) - 1; // start val is remaining items.
+}
+// else, we're not on last page, so render itemsPerPage items
+else {
+  startVaue = endVal + itemsPerPage - 1;
+}
+```
+
+- when we have 8 items it doesn't render anything, we end up with
